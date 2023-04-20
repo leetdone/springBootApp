060ecad128d0c919b75fad4f7d7826d92e25c305
leetdone <lindong_ye@163.com>
2023-04-20 00:34:51 -0400

    add some application.properties

--- src/main/java/com/luv2code/springboot/demo/myApp/rest/funRestController.java
+++ src/main/java/com/luv2code/springboot/demo/myApp/rest/funRestController.java
@@ -1,10 +1,15 @@
 package com.luv2code.springboot.demo.myApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class funRestController {
+
+    @Value("${lin.name}")
+    private String userName;
+
     //expose "/" that return "hello world"
     @GetMapping("/")
     public String sayHi(){
@@ -14,5 +19,10 @@ public class funRestController {
     @GetMapping("/dev")
     public String devTest(){
         return "dev Worksdgg?";
+    }
+
+    @GetMapping("yourName")
+    public String getName(){
+        return userName;
     }
 }
